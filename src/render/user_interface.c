dc996a64766be81c9a234ec651efbfe90d2d6ad6
Vladimir Bleskine <vbleskin@f2r2s8.clusters.42paris.fr>
2026-02-05 00:26:36 +0100

    fix: Enhance animation cycling and duration calculation robustness
    - Add `ft_print_anim_stack` debug utility for animation stack visualization.
    - Improve `ft_get_max_duration` with NULL checks for keyframe time arrays.
    - Refine `ft_prev_anim` logic to correctly cycle through animations, handling initial and wrap-around states.
    - Apply minor code style adjustments for consistency.

--- src/render/user_interface.c
+++ src/render/user_interface.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   user_interface.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vlad <vlad@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: vbleskin <vbleskin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 22:27:27 by vlad              #+#    #+#             */
-/*   Updated: 2026/02/04 15:42:10 by vlad             ###   ########.fr       */
+/*   Updated: 2026/02/04 23:33:53 by vbleskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,31 @@ int	ft_display_fps(t_fdf *data)
 	return (SUCCESS);
 }
 
-t_list  *ft_find_node(t_list *list, void *content_to_find)
+// *** DEBUG ***
+void	ft_print_anim_stack(t_list *list)
 {
-    while (list)
-    {
-        if (list->content == content_to_find)
-            return (list);
-        list = list->next;
-    }
-    return (NULL);
+	t_anim_stack	*stack;
+	int				i;
+
+	i = 0;
+	while (list)
+	{
+		stack = list->content;
+		i++;
+		printf("Anim %d : %s\n", i, stack->name);
+		list = list->next;
+	}
+}
+
+t_list	*ft_find_node(t_list *list, void *content_to_find)
+{
+	while (list)
+	{
+		if (list->content == content_to_find)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
 }
 
 int	ft_pause(t_fdf *data)
@@ -66,19 +82,19 @@ static double	ft_get_max_duration(t_anim_node *node, double max_duration)
 {
 	double	end;
 
-	if (node->x && node->x->n_keys > 0)
+	if (node->x && node->x->n_keys > 0 && node->y->time)
 	{
 		end = node->x->time[node->x->n_keys - 1];
 		if (end > max_duration)
 			max_duration = end;
 	}
-	if (node->y && node->y->n_keys > 0)
+	if (node->y && node->y->n_keys > 0 && node->y->time)
 	{
 		end = node->y->time[node->y->n_keys - 1];
 		if (end > max_duration)
 			max_duration = end;
 	}
-	if (node->z && node->z->n_keys > 0)
+	if (node->z && node->z->n_keys > 0 && node->y->time)
 	{
 		end = node->z->time[node->z->n_keys - 1];
 		if (end > max_duration)
@@ -89,27 +105,27 @@ static double	ft_get_max_duration(t_anim_node *node, double max_duration)
 
 double	ft_get_anim_duration(t_anim_stack *anim)
 {
-	t_list          *layers;
-    t_list          *nodes;
-    t_anim_layer    *layer;
-    double          max_duration;
+	t_list			*layers;
+	t_list			*nodes;
+	t_anim_layer	*layer;
+	double			max_duration;
 
-    max_duration = 0.0;
-    if (!anim)
-        return (0.0);
-    layers = anim->layers;
-    while (layers)
-    {
-        layer = (t_anim_layer *)layers->content;
-        nodes = layer->nodes;
-        while (nodes)
-        {
+	max_duration = 0.0;
+	if (!anim)
+		return (0.0);
+	layers = anim->layers;
+	while (layers)
+	{
+		layer = (t_anim_layer *)layers->content;
+		nodes = layer->nodes;
+		while (nodes)
+		{
 			max_duration = ft_get_max_duration((t_anim_node *)nodes->content, max_duration);
-            nodes = nodes->next;
-        }
-        layers = layers->next;
-    }
-    return (max_duration);
+			nodes = nodes->next;
+		}
+		layers = layers->next;
+	}
+	return (max_duration);
 }
 
 void	ft_next_anim(t_fdf *data)
@@ -118,10 +134,10 @@ void	ft_next_anim(t_fdf *data)
 
 	if (!data->fbx || !data->fbx->anim_stack)
 		return ;
-    current_node = ft_find_node(data->fbx->anim_stack, data->fbx->current_anim);
-    if (current_node && current_node->next)
+	current_node = ft_find_node(data->fbx->anim_stack, data->fbx->current_anim);
+	if (current_node && current_node->next)
 		data->fbx->current_anim = (t_anim_stack *)(current_node->next->content);
-    else if (data->fbx->anim_stack)
+	else if (data->fbx->anim_stack)
 		data->fbx->current_anim = (t_anim_stack *)(data->fbx->anim_stack->content);
 	data->timer.weighted_value = 0;
 	data->timer.duration = ft_get_anim_duration(data->fbx->current_anim);
@@ -133,10 +149,17 @@ void	ft_prev_anim(t_fdf *data)
 {
 	t_list	*temp;
 	t_list	*last;
-	
+
 	if (!data->fbx || !data->fbx->anim_stack)
 		return ;
-	else if (data->fbx->current_anim == NULL || (data->fbx->current_anim == (t_anim_stack *)data->fbx->anim_stack->content))
+	ft_print_anim_stack(data->fbx->anim_stack);
+	if (data->fbx->current_anim == NULL)
+	{
+		last = ft_lstlast(data->fbx->anim_stack);
+		if (last)
+			data->fbx->current_anim = (t_anim_stack *)last->content;
+	}
+	else if (data->fbx->current_anim == (t_anim_stack *)data->fbx->anim_stack->content)
 	{
 		last = ft_lstlast(data->fbx->anim_stack);
 		if (last)
